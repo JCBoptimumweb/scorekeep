3e2eac2bd57e1a649d6b510e31ccbeb11976b751
JCBoptimumweb <jeremy@optimumweb.com.au>
2017-07-26 16:06:38 +1000

    working app

--- client/main.js
+++ client/main.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
-const players = [{
-    _id: '1',
-    name: 'Lauren',
-    score: 99
-}, {
-    _id: '2',
-    name: 'Cory',
-    score: 43
-    }, {
-    _id: '3',
-    name: 'Bert',
-    score: 1
-}];
+import {Players, calculatePlayerPositions} from './../imports/api/players'
+import App from './../imports/ui/App';
 
-const renderPlayers = function (playersList) {
 
-    return playersList.map(function (players) {
-        return <p key={players._id}>{players.name} has {players.score} points</p>;
+Meteor.startup(() => {
+    Tracker.autorun(() => {
+        let players = Players.find({}, {sort: {score: -1}}).fetch();
+        let positionedPlayers = calculatePlayerPositions(players);
+        let title = 'Score Keep';
+        ReactDom.render(<App title={title} players={positionedPlayers}/>, document.getElementById('app'))
     });
-}
-Meteor.startup(function () {
-    let title = 'Score Keep';
-    let name = 'Jeremy';
-    let jsx = (
-        <div>
-            <h1> {title} </h1>
-            <p> Hello {name}!</p>
-            <p>This is my second p</p>
-            {renderPlayers(players)}
-        </div>
-    );
-    ReactDom.render(jsx, document.getElementById('app'))
+
 });
--- server/main.js
+++ server/main.js
@@ -2,10 +2,5 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 
-Meteor.startup(function () {
-    Players.insert({
-        name: 'vikram',
-        score: 22
-        });
-    console.log(Players.find().fetch());
+Meteor.startup(() => {
 });
